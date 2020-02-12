243dc0df20dbcbbf1e988e59ba51230eac272863
Nicholas Patel <np4795@yahoo.com>
2020-02-12 05:25:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,55 @@
 def create_an_empty_array
+
   []
 end
 
+
 def create_an_array
-  mycolors=["gold", "silver", "violet", "gray"]
-end
 
-def add_element_to_end_of_array(array, element)
-  mycolors<<"white"
+  mycolors = ["gold", "silver", "violet", "gray"]
+end	
+
+
+def add_element_to_end_of_array(array, element)	
+
+  mycolors << "white"
   puts mycolors.inspect
-end
+end	
 
-def add_element_to_start_of_array(array, element)
-  mycolors.unshift="purple"
+
+def add_element_to_start_of_array(array, element)	
+
+  mycolors.unshift = "purple"
   puts mycolors.inspect
-end
+end	
 
-def remove_element_from_end_of_array(array)
+
+def remove_element_from_end_of_array(array)	
+
   mycolors.pop
   puts mycolors.inspect
 end
 
-def remove_element_from_start_of_array(array)
+
+def remove_element_from_start_of_array(array)	
   mycolors.shift
   puts mycolors.inspect
-end
+end	
 
-def retrieve_element_from_index(array, index_number)
+
+def retrieve_element_from_index(array, index_number)	
+
   mycolors[2]
-end
+end	
 
-def retrieve_first_element_from_array(array)
+
+def retrieve_first_element_from_array(array)	
+
   mycolors[0]
-end
+end	
 
-def retrieve_last_element_from_array(array)
+
+def retrieve_last_element_from_array(array)	d
+
   mycolors[-1]
-end
+end	
