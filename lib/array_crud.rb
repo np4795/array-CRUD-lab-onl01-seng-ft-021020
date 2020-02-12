99eba31a72a6a3607780a058aadc523dabeae1d9
Nicholas Patel <np4795@yahoo.com>
2020-02-12 05:24:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,23 +3,27 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  mycolors=["gold", "silver", "violet", "gray"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  mycolors<<"white"
+  puts mycolors.inspect
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  mycolors.unshift="purple"
+  puts mycolors.inspect
 end
 
 def remove_element_from_end_of_array(array)
-  
+  mycolors.pop
+  puts mycolors.inspect
 end
 
 def remove_element_from_start_of_array(array)
-  
+  mycolors.shift
+  puts mycolors.inspect
 end
 
 def retrieve_element_from_index(array, index_number)
